c7f6708f1978a8ab193d96038e531932c7d0a5be
hangyejiadao <953917332@qq.com>
2018-01-13 10:58:01 +0800

    在v现在v操作需从v从

--- Server/Server/Form1.cs
+++ Server/Server/Form1.cs
@@ -4,7 +4,10 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +18,106 @@ namespace Server
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private IPAddress myIP;
+        private IPEndPoint MyServer;
+        private Socket sock;
+        private bool check;
+        private Socket accSocket;
+        private void btnStartListen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                myIP = IPAddress.Parse(txtHost.Text.Trim());
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("你输入的IP地址格式不正确,请重新输入!");
+            }
+            try
+            {
+                ThreadPool.QueueUserWorkItem(p =>
+                {
+                    accp();
+                });
+            }
+            catch (Exception exception)
+            {
+                rtbServerState.AppendText(exception.Message + "\r\n");
+            }
+        }
+
+        private void accp()
+        {
+            MyServer = new IPEndPoint(myIP, Int32.Parse(txtPort.Text));
+            sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sock.Bind(MyServer);
+            sock.Listen(Int32.Parse(txtPort.Text));
+            Invoke(new Action(() =>
+            {
+                rtbServerState.AppendText("主机:" + txtHost.Text + " 端口:" + txtPort.Text + "  开始监听........\r\n");
+                btnStartListen.Enabled = false;
+            }));
+            accSocket = sock.Accept();
+            while (true)
+            {
+      
+                if (accSocket.Connected)
+                {
+                    Invoke(new Action(() =>
+                    {
+                        rtbServerState.AppendText("与客户建立连接\r\n");
+                    }));
+                    while (true)
+                    {
+
+                        byte[] recbytes = new byte[1024];
+                        NetworkStream netStream = new NetworkStream(accSocket);
+                        netStream.Read(recbytes, 0, recbytes.Length);
+                        string RecMessage = System.Text.Encoding.BigEndianUnicode.GetString(recbytes);
+                        Invoke(new Action(() =>
+                        {
+                            txtRecMsg.AppendText(RecMessage + "\r\n");
+                            txtRecMsg.AppendText("\r\n");
+                        }));
+                    }
+                }
+            }
+            
+        }
+
+
+
+
+        private void btnSendMsg_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                byte[] sendBytes = new byte[64];
+                string send = txtSendMsg.Text + "\r\n";
+                NetworkStream netStream = new NetworkStream(accSocket);
+                sendBytes = System.Text.Encoding.BigEndianUnicode.GetBytes(send.ToCharArray());
+                netStream.Write(sendBytes, 0, sendBytes.Length);
+            }
+            catch (Exception exception)
+            {
+                rtbServerState.AppendText("连接尚未建立！无法发送!\r\n");
+            }
+        }
+
+        private void btnStopListen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sock.Close();
+                rtbServerState.AppendText("主机:" + txtHost.Text + " 端口" + txtPort.Text + " 停止监听:\r\n");
+
+            }
+            catch
+            {
+                rtbServerState.AppendText("监听尚未开始,关闭无效！\r\n");
+            }
         }
     }
 }
