0d69828087508c806bfb9175d7f1afea2a1fea7b
hangyejiadao <953917332@qq.com>
2018-01-17 22:06:01 +0800

    Init

--- RabbitMq/Send/Send/Form1.Designer.cs
+++ RabbitMq/Send/Send/Form1.Designer.cs
@@ -23,92 +23,70 @@
         #region Windows 窗体设计器生成的代码
 
         /// <summary>
-        /// 设计器支持所需的方法 - 不要修改
+        /// 设计器支持所需的方法 - 不要
         /// 使用代码编辑器修改此方法的内容。
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnSend = new System.Windows.Forms.Button();
-            this.txtSend = new System.Windows.Forms.TextBox();
-            this.btnConnect = new System.Windows.Forms.Button();
-            this.txtHost = new System.Windows.Forms.TextBox();
-            this.txtPort = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // btnSend
+            // button1
             // 
-            this.btnSend.Location = new System.Drawing.Point(12, 133);
-            this.btnSend.Name = "btnSend";
-            this.btnSend.Size = new System.Drawing.Size(75, 23);
-            this.btnSend.TabIndex = 0;
-            this.btnSend.Text = "Send";
-            this.btnSend.UseVisualStyleBackColor = true;
-            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
-            // 
-            // txtSend
-            // 
-            this.txtSend.Location = new System.Drawing.Point(12, 77);
-            this.txtSend.Multiline = true;
-            this.txtSend.Name = "txtSend";
-            this.txtSend.Size = new System.Drawing.Size(471, 25);
-            this.txtSend.TabIndex = 1;
-            // 
-            // btnConnect
-            // 
-            this.btnConnect.Location = new System.Drawing.Point(422, 31);
-            this.btnConnect.Name = "btnConnect";
-            this.btnConnect.Size = new System.Drawing.Size(75, 23);
-            this.btnConnect.TabIndex = 2;
-            this.btnConnect.Text = "建立连接";
-            this.btnConnect.UseVisualStyleBackColor = true;
-            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
-            // 
-            // txtHost
-            // 
-            this.txtHost.Location = new System.Drawing.Point(96, 31);
-            this.txtHost.Name = "txtHost";
-            this.txtHost.Size = new System.Drawing.Size(100, 25);
-            this.txtHost.TabIndex = 3;
-            // 
-            // txtPort
-            // 
-            this.txtPort.Location = new System.Drawing.Point(293, 31);
-            this.txtPort.Name = "txtPort";
-            this.txtPort.Size = new System.Drawing.Size(100, 25);
-            this.txtPort.TabIndex = 4;
+            this.button1.Location = new System.Drawing.Point(380, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "建立连接";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 38);
+            this.label1.Location = new System.Drawing.Point(29, 22);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 15);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "服务器名:";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "服务端";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(77, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(219, 37);
+            this.label2.Location = new System.Drawing.Point(201, 22);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(45, 15);
-            this.label2.TabIndex = 6;
-            this.label2.Text = "端口:";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "端口";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(237, 17);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.TabIndex = 4;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(522, 333);
+            this.ClientSize = new System.Drawing.Size(602, 386);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtPort);
-            this.Controls.Add(this.txtHost);
-            this.Controls.Add(this.btnConnect);
-            this.Controls.Add(this.txtSend);
-            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -118,13 +96,11 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btnSend;
-        private System.Windows.Forms.TextBox txtSend;
-        private System.Windows.Forms.Button btnConnect;
-        private System.Windows.Forms.TextBox txtHost;
-        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- RabbitMq/Send/Send/Form1.cs
+++ RabbitMq/Send/Send/Form1.cs
@@ -7,34 +7,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using RabbitMQ.Client;
 
 namespace Send
 {
     public partial class Form1 : Form
     {
-
         public Form1()
         {
             InitializeComponent();
         }
-        private ConnectionFactory factory;
-        private IConnection conn;
-        private IModel channel;
-        private void btnSend_Click(object sender, EventArgs e)
-        {
-            byte[] body = Encoding.UTF8.GetBytes(txtSend.Text);
-            channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
-        }
 
-        private void btnConnect_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            factory = new ConnectionFactory() { HostName = txtHost.Text };
-            conn = factory.CreateConnection(); 
-            channel = conn.CreateModel(); 
-            channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false,   arguments: null);
-            txtHost.Enabled = false;
-            btnConnect.Enabled = false;
+
         }
     }
 }
